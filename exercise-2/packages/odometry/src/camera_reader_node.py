4e4dd938176b7c2e5f87fd064acbc1f7550dd4d5
Antonio <antonio2@ualberta.ca>
2025-02-12 16:02:27 -0700

    camera implementation done for now

---
+++ exercise-2/packages/odometry/src/camera_reader_node.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+
+import rospy
+from os import uname
+from re import match
+from duckietown.dtros import DTROS, NodeType
+from sensor_msgs.msg import CompressedImage, Image
+
+import cv2
+from cv_bridge import CvBridge
+
+class CameraReaderNode(DTROS):
+
+    def __init__(self, node_name):
+        # initialize the DTROS parent class
+        super(CameraReaderNode, self).__init__(node_name=node_name, node_type=NodeType.VISUALIZATION)
+        # static parameters
+        self._hostname = uname()[1]
+
+        if not match(r"^csc\d+$", self._hostname):
+            print("Pass in the hostname of the bot ![DUCKIEBOT]:")
+            self._hostname = input()
+        
+        self._vehicle_name = self._hostname
+        self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
+        self._custom_topic = f"/annotated_image"
+        # bridge between OpenCV and ROS
+        self._bridge = CvBridge()
+
+        # create window
+        # self._window = "camera-reader"
+        # cv2.namedWindow(self._window, cv2.WINDOW_AUTOSIZE)
+
+        # construct subscriber
+        self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
+        self.pub = rospy.Publisher(self._custom_topic, Image, queue_size=10)
+
+    def callback(self, msg):
+        # convert JPEG bytes to CV image
+        image = self._bridge.compressed_imgmsg_to_cv2(msg)
+        width, height, _ = image.shape
+        print("width:", width, "height:", height)
+        
+        # convert to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        size_text = f"{width}X{height}"
+
+        # Format the annotation text
+        annotation_text = f"Duck {self._hostname} says, 'Cheese! Capturing {size_text} - quack-tastic!'"
+
+        # Annotate the image
+        annotated_image = cv2.putText(
+            gray_image,
+            annotation_text,
+            (10, 100),  # Bottom-left corner for the text
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,  # Font size
+            (0, 0, 0),  # White text for grayscale image
+            1,  # Thickness
+            cv2.LINE_AA
+        )
+
+        # Convert back to ROS Image message
+        annotated_msg = self._bridge.cv2_to_imgmsg(annotated_image)
+        
+        self.pub.publish(annotated_msg)
+
+if __name__ == '__main__':
+    # create the node
+    node = CameraReaderNode(node_name='camera_reader_node')
+    # keep spinning
+    rospy.spin()
